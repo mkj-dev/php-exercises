394819267e9b8b46bb27dafd8b182c218b13ed72
mkj-dev <56467077+mkj-dev@users.noreply.github.com>
2023-05-08 20:32:08 +0000

    Include functions.php, add .error and .valid class styles

--- index.php
+++ index.php
@@ -3,11 +3,19 @@
     <title>PHP Exercises</title>
     <style>
       body { text-align: center; }
+      .title {
+        font-size: 36px;
+        border-bottom: 2px solid #444;
+      }
+      .error { color: red; }
+      .valid { color: green; }
     </style>
   </head>
   <body>
   <?php
-    echo "<h2>Control structures</h2>";
+    echo "<h2 class='title'>Control structures</h2>";
     include_once('./control-structures.php');
+    echo "<h2 class='title'>Functions</h2>";
+    include_once('./functions.php');
   ?>
 </html>
